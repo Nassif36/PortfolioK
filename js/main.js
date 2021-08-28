77e0cd6dcfebbc19b47bb85755d79ddd821c4df2
Nassif36 <khalilyoutuber@hotmail.com>
2021-08-28 12:30:59 -0300

    slide

--- js/main.js
+++ js/main.js
@@ -1,4 +1,19 @@
 
+    // Navigation 
+
+    var menu = document.querySelector('.barra_nav');
+    var menuBtn = document.querySelector(".barra_logo button");
+
+    menuBtn.addEventListener('click', () => {
+        menu.classList.toggle('opened');
+    });
+    var menuLink = document.querySelectorAll(".nav li");
+    menuLink.forEach(menuLink => menuLink.addEventListener('click', () => {
+        menu.classList.toggle('opened');
+    }));
+
+
+    // Theme Switcher
    const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 
    function switchTheme(e) {
@@ -8,21 +23,11 @@
            document.documentElement.setAttribute('data-theme', 'light');
        }
    }
-
    toggleSwitch.addEventListener('change', switchTheme, false);
 
 
-   var menu = document.querySelector('.barra');
-   var menuBtn = document.querySelector(".barra button");
-   
-   menuBtn.addEventListener('click', () => {
-       menu.classList.toggle('opened');
-   });
-   var menuLink = document.querySelectorAll(".nav_links li");
-   menuLink.forEach(menuLink => menuLink.addEventListener('click', () => {
-       menu.classList.toggle('opened');
-   }));
-
+     
+   // On Scroll Navbar
     var prevScrollpos = window.pageYOffset;
     window.onscroll = function () {
         var currentScrollPos = window.pageYOffset;
